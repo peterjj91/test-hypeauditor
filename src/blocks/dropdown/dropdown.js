cf51d35c4196134e99d8ddb827d9ef466e0635c2
Popeskul Pavel <peterjj91@gmail.com>
2019-07-22 01:02:42 +0300

    complete test

---
+++ src/blocks/dropdown/dropdown.js
@@ -0,0 +1,37 @@
+// document.addEventListener('DOMContentLoaded', function(){});
+// (function(){
+// код
+// }());
+(function(){
+  var elements = document.querySelectorAll(".dropdown-toggle");
+
+  function openDropdown(event){
+    event.target.parentNode.classList.add("open");
+  }
+
+  function resetDropdowns(){
+    var element = document.querySelector(".dropdown.open");
+    if(element){ element.classList.remove("open"); }
+  }
+
+  function documentHandler(){
+    resetDropdowns();
+    document.removeEventListener('click', documentHandler , false); //remove document click listener
+  };
+
+  if(elements.length>0){
+    elements.forEach(el=>{
+      el.addEventListener('click',function(e){
+        var isActive = e.target.parentNode.classList.contains("open");
+        resetDropdowns();
+        if (isActive) {
+          return;
+        }
+        e.stopPropagation();
+        e.preventDefault();
+        openDropdown(e);
+        document.addEventListener('click', documentHandler, false); //add document click listener to close dropdown on outside click
+      }, false);
+    })
+  }
+})()
---
+++ src/blocks/slider/slider.js
@@ -0,0 +1,16 @@
+var slider = tns({
+  container: '.main-slider',
+  autoWidth: true,
+  loop: false,
+  // fixedWidth: 280,
+  items: 4.5,
+  gutter: 17,
+  // controls: false,
+  // edgePadding: 50,
+  lazyload: true,
+  // slideBy: 'page',
+  nav: false,
+  mouseDrag: true,
+  autoplay: false,
+  speed: 400
+});
